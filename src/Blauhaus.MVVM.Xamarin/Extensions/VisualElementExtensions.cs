22a0fe67c17b4317c37d06c587f77663c07da35d
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-08 12:17:17 +0200

    no message

--- src/Blauhaus.MVVM.Xamarin/Extensions/VisualElementExtensions.cs
+++ src/Blauhaus.MVVM.Xamarin/Extensions/VisualElementExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Windows.Input;
 using Blauhaus.Common.Utils.Extensions;
 using Blauhaus.MVVM.Abstractions.Commands;
 using Blauhaus.MVVM.Abstractions.ViewModels;
@@ -9,12 +10,11 @@ namespace Blauhaus.MVVM.Xamarin.Extensions
 {
     public static class VisualElementExtensions
     {
-        public static TControl BindTapToParent<TViewModel, TControl>(this TControl control, Expression<Func<TViewModel, IExecutingCommand>> viewModelCommand) 
+        public static TControl BindTapToParent<TViewModel, TControl>(this TControl control, Expression<Func<TViewModel, ICommand>> viewModelCommand) 
             where TViewModel : IViewModel
             where TControl : View
-            
         {
-            var commandName = viewModelCommand.ToPropertyName() + "." + nameof(IExecutingCommand);
+            var commandName = viewModelCommand.ToPropertyName();
 
             var tapGestureRecognizer = new TapGestureRecognizer();
             var bindingSource = new RelativeBindingSource(RelativeBindingSourceMode.FindAncestorBindingContext, typeof(TViewModel));
