4f0f90379d42ac624905fdaff3cd8cf8ddbb55b8
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-09 12:40:51 +0200

    no message

--- src/Blauhaus.MVVM.Abstractions/Contracts/IInitialize.cs
+++ src/Blauhaus.MVVM.Abstractions/Contracts/IInitialize.cs
@@ -5,6 +5,8 @@ namespace Blauhaus.MVVM.Abstractions.Contracts
 {
     public interface IInitialize<in T>
     {
+        //NB this does not return Task because time-consuming setup in ViewModels should be handled in AppearCommand
+
         void Initialize(T initialValue);
     }
 }
