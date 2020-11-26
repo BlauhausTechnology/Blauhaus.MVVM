efec0fc0c46a635e89bfe0363d399e5738cfe60e
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-26 14:22:00 +0200

    no message

---
+++ src/Blauhaus.MVVM.Xamarin/Views/ContentViews/BaseContentView.cs
@@ -0,0 +1,20 @@
+﻿using Blauhaus.MVVM.Abstractions.Contracts;
+using Blauhaus.MVVM.Abstractions.Views;
+using Blauhaus.MVVM.Xamarin.Views.Content;
+
+namespace Blauhaus.MVVM.Xamarin.Views.ContentViews
+{
+    public class BaseContentView<TViewModel> : BaseUpdateContentView, IView
+    {
+        protected readonly TViewModel ViewModel;
+
+        protected BaseContentView(TViewModel viewModel) 
+            : base(viewModel is IUpdate)
+        {
+            ViewModel = viewModel;
+            BindingContext = ViewModel;
+        }
+         
+
+    }
+}
