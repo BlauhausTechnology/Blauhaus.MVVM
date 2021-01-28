87b2b4a59913c1b2e5fc6c1063b3448386cac0e3
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-01-28 08:41:25 +0200

    no message

---
+++ src/Blauhaus.MVVM.TestHelpers/MockBuilders/Services/LocalizationServiceMockBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using Blauhaus.MVVM.Abstractions.Localization;
+using Blauhaus.TestHelpers.MockBuilders;
+
+namespace Blauhaus.MVVM.TestHelpers.MockBuilders.Services
+{
+    public class LocalizationServiceMockBuilder : BaseMockBuilder<LocalizationServiceMockBuilder, ILocalizationService>
+    {
+        public LocalizationServiceMockBuilder Where_GetCulture_returns(CultureInfo cultureInfo)
+        {
+            Mock.Setup(x => x.GetCulture()).Returns(cultureInfo);
+            return this;
+        }
+    }
+}
---
+++ src/Blauhaus.MVVM/Localization/LocalizationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Threading;
+using Blauhaus.MVVM.Abstractions.Localization;
+
+namespace Blauhaus.MVVM.Localization
+{
+    public class LocalizationService : ILocalizationService
+    {
+        public void SetCulture(string cultureString)
+        {
+            var currentCulture = new CultureInfo(cultureString);
+            Thread.CurrentThread.CurrentCulture = currentCulture;
+            Thread.CurrentThread.CurrentUICulture = currentCulture;
+        }
+
+        public CultureInfo GetCulture()
+        {
+            return Thread.CurrentThread.CurrentUICulture;
+        }
+    }
+}
