0cafe0d925d903d842cda165db205204d29b7a99
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-01-28 08:43:43 +0200

    no message

--- src/Blauhaus.MVVM/Localization/LocalizationService.cs
+++ src/Blauhaus.MVVM/Localization/LocalizationService.cs
@@ -1,16 +1,27 @@
 ﻿using System.Globalization;
 using System.Threading;
+using Blauhaus.Analytics.Abstractions.Service;
 using Blauhaus.MVVM.Abstractions.Localization;
 
 namespace Blauhaus.MVVM.Localization
 {
     public class LocalizationService : ILocalizationService
     {
+        private readonly IAnalyticsService _analyticsService;
+
+        public LocalizationService(IAnalyticsService analyticsService)
+        {
+            _analyticsService = analyticsService;
+        }
+
         public void SetCulture(string cultureString)
         {
+            var originalCulture = GetCulture().DisplayName;
             var currentCulture = new CultureInfo(cultureString);
             Thread.CurrentThread.CurrentCulture = currentCulture;
             Thread.CurrentThread.CurrentUICulture = currentCulture;
+
+            _analyticsService.Trace(this, $"Set device culture from {originalCulture} to {currentCulture.DisplayName}");
         }
 
         public CultureInfo GetCulture()
