e83252d1aed62c99ace634ce530ca6a68c8496df
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-02-10 09:16:06 +0200

    no message

--- src/Blauhaus.MVVM.Xamarin/ErrorHandling/ErrorHandler.cs
+++ src/Blauhaus.MVVM.Xamarin/ErrorHandling/ErrorHandler.cs
@@ -26,22 +26,22 @@ namespace Blauhaus.MVVM.Xamarin.ErrorHandling
         }
 
 
-        public Task HandleExceptionAsync(object sender, Exception exception)
+        public async Task HandleExceptionAsync(object sender, Exception exception)
         {
-
             _analyticsService.LogException(sender, exception);
 
             var errorMessage = "An unexpected error has occured";
             if (exception is ErrorException errorException)
             {
                 errorMessage = errorException.Error.Description;
+                await OnErrorHandledAsync(errorException.Error);
             }
             else if ((BuildConfig) _buildConfig == BuildConfig.Debug)
             {
                 errorMessage = exception.Message;
             }
 
-            return _dialogService.DisplayAlertAsync("Error", errorMessage);
+            await _dialogService.DisplayAlertAsync("Error", errorMessage);
         }
 
         public async Task HandleErrorAsync(string errorMessage)
@@ -49,13 +49,20 @@ namespace Blauhaus.MVVM.Xamarin.ErrorHandling
             if (errorMessage.IsError(out var error))
             {
                 errorMessage = error.Description;
+                await OnErrorHandledAsync(error);
             }
             await _dialogService.DisplayAlertAsync("Error", errorMessage);
         }
 
         public async Task HandleErrorAsync(Error error)
         {
+            await OnErrorHandledAsync(error);
             await _dialogService.DisplayAlertAsync("Error", error.Description);
+        }
+
+        protected virtual Task OnErrorHandledAsync(Error error)
+        {
+            return Task.CompletedTask;
         }
     }
 }
