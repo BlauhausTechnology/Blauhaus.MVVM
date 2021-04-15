2af872683099419df4c12bbbf9b8efeff7c79252
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-04-15 09:59:34 +0200

    no message

---
+++ src/Blauhaus.MVVM.Abstractions/ViewModels/Tabs/ITabbedViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.MVVM.Abstractions.ViewModels.Tabs
+{
+    public interface ITabbedViewModel
+    {
+        IReadOnlyCollection<TabDefinition> TabDefinitions { get; }
+    }
+}
