45b56d478898d8fb5981e0c8fdcc8313efe47be7
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-05-10 14:23:16 +0200

    no message

---
+++ src/Blauhaus.MVVM.Xamarin/Converters/FuncConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Blauhaus.MVVM.Xamarin.Converters
+{
+    public class FuncConverter<TParameter> : IValueConverter
+    {
+        private readonly Func<TParameter, object> _func;
+
+        public FuncConverter(Func<TParameter, object> func)
+        {
+            _func = func;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var actionParameter = (TParameter)value;
+            return _func?.Invoke(actionParameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
