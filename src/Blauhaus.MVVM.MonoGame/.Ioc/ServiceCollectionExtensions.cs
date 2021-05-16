78a216eaa219741a1a4dac44330859cde2f417b2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-05-16 13:08:35 +0200

    no message

--- src/Blauhaus.MVVM.MonoGame/.Ioc/ServiceCollectionExtensions.cs
+++ src/Blauhaus.MVVM.MonoGame/.Ioc/ServiceCollectionExtensions.cs
@@ -8,7 +8,7 @@ using Blauhaus.MVVM.MonoGame.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace Blauhaus.MVVM.MonoGame._Ioc
+namespace Blauhaus.MVVM.MonoGame.Ioc
 {
     public static class ServiceCollectionExtensions
     {
@@ -16,13 +16,19 @@ namespace Blauhaus.MVVM.MonoGame._Ioc
         
         public static IServiceCollection AddMvvmServices(this IServiceCollection services)
         {
+            return services.AddMvvmServices<MonoGameErrorHandler>();
+        }
+
+        public static IServiceCollection AddMvvmServices<TErrorHandler>(this IServiceCollection services)
+            where TErrorHandler : class, IErrorHandler
+        {
             services.AddSingleton<INavigationService, MonoGameNavigationService>();
             
             services
                 .AddSingleton<IDialogService, MonoGameDialogService>()
                 .AddSingleton<INavigationService, MonoGameNavigationService>()
                 .AddSingleton<INavigationLookup>(x => NavigationLookup)
-                .AddSingleton<IErrorHandler, MonoGameErrorHandler>();
+                .AddSingleton<IErrorHandler, TErrorHandler>();
             
             return services;
         }
