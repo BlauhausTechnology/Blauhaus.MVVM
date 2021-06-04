83db6f351edca405693b3ce4cdd75bb4bfc583d1
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-06-04 11:25:55 +0200

    no message

--- src/Blauhaus.MVVM.TestHelpers/MockBuilders/Services/DialogServiceMockBuilder.cs
+++ src/Blauhaus.MVVM.TestHelpers/MockBuilders/Services/DialogServiceMockBuilder.cs
@@ -6,6 +6,14 @@ namespace Blauhaus.MVVM.TestHelpers.MockBuilders.Services
 {
     public class DialogServiceMockBuilder : BaseMockBuilder<DialogServiceMockBuilder, IDialogService>
     {
+
+        public DialogServiceMockBuilder Where_DisplayConfirmationAsync_returns(bool result)
+        {
+            Mock.Setup(x => x.DisplayConfirmationAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(result);
+            return this;
+        }
+
         public void Verify_DisplayAlert_called(string title, string message, string accept)
         {
             Mock.Verify(x => x.DisplayAlertAsync(title, message, accept));
