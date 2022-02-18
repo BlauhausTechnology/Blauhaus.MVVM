687c14a33dbb2723b59da40ee682e0b6629ca9c7
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-02-18 10:29:25 +0200

    no message

--- src/Blauhaus.MVVM.Abstractions/ViewModels/BaseViewModel.cs
+++ src/Blauhaus.MVVM.Abstractions/ViewModels/BaseViewModel.cs
@@ -8,23 +8,6 @@ namespace Blauhaus.MVVM.Abstractions.ViewModels
 {
     public abstract class BaseViewModel : BaseBindableObject, IViewModel
     {
-        protected readonly IServiceLocator ServiceLocator;
-        protected readonly IAnalyticsService AnalyticsService;
-        protected readonly IErrorHandler ErrorHandler;
-        protected readonly INavigationService NavigationService;
         
-        protected T Resolve<T>() where T : class => ServiceLocator.Resolve<T>();
-
-        protected BaseViewModel(
-            IServiceLocator serviceLocator, 
-            IAnalyticsService analyticsService, 
-            IErrorHandler errorHandler, 
-            INavigationService navigationService)
-        {
-            ServiceLocator = serviceLocator;
-            AnalyticsService = analyticsService;
-            ErrorHandler = errorHandler;
-            NavigationService = navigationService;
-        }
     }
 }
