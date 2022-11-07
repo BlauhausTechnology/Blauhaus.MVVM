5fa2ab2431e409a8775606f5092a94faa0b2a8e1
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-11-07 14:47:53 +0200

    no message

---
+++ src/Blauhaus.MVVM/ValueProperties/DecimalValueProperty.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using Blauhaus.MVVM.ValueProperties.Base;
+
+namespace Blauhaus.MVVM.ValueProperties;
+
+public class DecimalValueProperty : BaseValueProperty<decimal?>
+{
+    protected DecimalValueProperty(string name, decimal? value, bool isVisible = true) : base(name, value, isVisible)
+    {
+    }
+
+    public static DecimalValueProperty Create(decimal? value = null, [CallerMemberName] string name = "")
+    {
+        return new DecimalValueProperty(name, value, true);
+    }
+    public static DecimalValueProperty CreateInvisible(decimal? value = null, [CallerMemberName] string name = "")
+    {
+        return new DecimalValueProperty(name, value, false);
+    }
+}
