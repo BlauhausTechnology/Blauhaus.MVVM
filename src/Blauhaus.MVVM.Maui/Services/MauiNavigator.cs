42cb05cd34a98646d8436d4bd4ebf70c6f9518b4
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-12-29 09:08:17 +0200

    no message

--- src/Blauhaus.MVVM.Maui/Services/MauiNavigator.cs
+++ src/Blauhaus.MVVM.Maui/Services/MauiNavigator.cs
@@ -1,10 +1,19 @@
-﻿using Blauhaus.MVVM.Abstractions.TargetNavigation;
+﻿using Blauhaus.Analytics.Abstractions;
+using Blauhaus.MVVM.Abstractions.TargetNavigation;
+using Microsoft.Extensions.Logging;
 using IView = Blauhaus.MVVM.Abstractions.Views.IView;
 
 namespace Blauhaus.MVVM.Maui.Services;
 
 public class MauiNavigator : IPlatformNavigator
 {
+    private readonly IAnalyticsLogger<MauiNavigator> _logger;
+
+    public MauiNavigator(IAnalyticsLogger<MauiNavigator> logger)
+    {
+        _logger = logger;
+    }
+
     public IView? GetCurrentMainView()
     {
         if (Application.Current is null) return null;
@@ -17,6 +26,22 @@ public class MauiNavigator : IPlatformNavigator
     {
         if (Application.Current is null) throw new InvalidOperationException("Cannot set application main page because current application is null");
         if(view is not Page mauiPage) throw new InvalidOperationException($"View {view.GetType().Name} is not a valid Maui page type");
-        Application.Current.MainPage = mauiPage;
+
+        _logger.LogDebug("Showing page {PageName}", view.GetType().Name);
+
+        if (MainThread.IsMainThread)
+        {
+            Application.Current.MainPage = mauiPage;
+        }
+
+        else
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                Application.Current.MainPage = mauiPage;
+            });
+        }
+        
+        
     }
 }
