409f1bad7e4a8f859e3c0cff5ea50023e0c26ed8
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-01-16 11:18:08 +0200

    no message

---
+++ src/Blauhaus.MVVM.Maui/Services/HotReloadService.cs
@@ -0,0 +1,15 @@
+﻿
+using Blauhaus.MVVM.Maui.Services;
+
+[assembly: System.Reflection.Metadata.MetadataUpdateHandlerAttribute(typeof(HotReloadService))]
+namespace Blauhaus.MVVM.Maui.Services { 
+    public static class HotReloadService
+    {
+        public static event Action<Type[]?>? UpdateApplicationEvent;
+
+        internal static void ClearCache(Type[]? types) { }
+        internal static void UpdateApplication(Type[]? types) {
+            UpdateApplicationEvent?.Invoke(types);
+        }
+    }
+}
--- src/Blauhaus.MVVM.Maui/Views/BaseMauiContentPage.cs
+++ src/Blauhaus.MVVM.Maui/Views/BaseMauiContentPage.cs
@@ -1,20 +1,34 @@
-﻿using Blauhaus.MVVM.Abstractions.ViewModels;
+﻿using Blauhaus.Common.Abstractions;
+using Blauhaus.MVVM.Abstractions.TargetNavigation;
+using Blauhaus.MVVM.Abstractions.ViewModels;
 using Blauhaus.MVVM.Abstractions.Views;
+using Blauhaus.MVVM.Maui.Services;
 
 namespace Blauhaus.MVVM.Maui.Views;
 
-public abstract class BaseMauiContentPage<TViewModel> : ContentPage, IView<TViewModel> where TViewModel : IViewModel
+public abstract class BaseMauiContentPage<TViewModel> : ContentPage, IView<TViewModel>, IAsyncInitializable<NavigationTarget>
+    where TViewModel : IViewModel
 {
     public TViewModel ViewModel { get; }
+    
+    private bool _isSubscribedToHotReload;
+    protected abstract bool IsHotReloadEnabled { get; }
 
     protected BaseMauiContentPage(TViewModel viewModel)
     {
         ViewModel = viewModel;
         BindingContext = ViewModel;
     }
-
-    protected override void OnAppearing()
+    
+    public virtual Task InitializeAsync(NavigationTarget value)
     {
+        return Task.CompletedTask;
+    }
+    
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
         if (ViewModel is IAppearingViewModel appearViewModel)
         {
             if (appearViewModel.AppearCommand.CanExecute(null))
@@ -22,10 +36,15 @@ public abstract class BaseMauiContentPage<TViewModel> : ContentPage, IView<TView
                 appearViewModel.AppearCommand.Execute();
             }
         }
+        
+        SubscribeToHotReload();
+
     }
 
-    protected override void OnDisappearing()
+    protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
     {
+        base.OnNavigatedFrom(args);
+
         if (ViewModel is IDisappearingViewModel disappearViewModel)
         {
             if (disappearViewModel.DisappearCommand.CanExecute(null))
@@ -33,6 +52,42 @@ public abstract class BaseMauiContentPage<TViewModel> : ContentPage, IView<TView
                 disappearViewModel.DisappearCommand.Execute();
             }
         }
+
+        UnsubscribeFromHotReload();
     }
+
+     
+    #region HotReload
+    private void SubscribeToHotReload()
+    {
+        if (IsHotReloadEnabled)
+        {
+            if (!_isSubscribedToHotReload)
+            {
+                HotReloadService.UpdateApplicationEvent += ReloadUi;
+                _isSubscribedToHotReload = true;
+            }
+        }
+       
+        Content = Build();
+    }
+
+    private void UnsubscribeFromHotReload()
+    {
+        if (IsHotReloadEnabled)
+        {
+            HotReloadService.UpdateApplicationEvent -= ReloadUi;
+            _isSubscribedToHotReload = false;
+        }
+    }
+
+    private void ReloadUi(Type[]? obj)
+    {
+        MainThread.BeginInvokeOnMainThread(() => Content = Build());
+    }
+
+    protected abstract View Build();
+
+    #endregion
 
 }
