a9209268a7e120d995c17e4b8e5f26d6fd8d5816
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-07-19 12:58:25 +0200

    no message

--- src/Blauhaus.MVVM.Abstractions/TargetNavigation/INavigator.cs
+++ src/Blauhaus.MVVM.Abstractions/TargetNavigation/INavigator.cs
@@ -1,8 +1,12 @@
 ﻿using System.Threading.Tasks;
+using Blauhaus.MVVM.Abstractions.Views;
 
 namespace Blauhaus.MVVM.Abstractions.TargetNavigation;
 
 public interface INavigator
 {
     Task NavigateAsync(NavigationTarget target);
+
+    Task<INavigationContainerView> GetContainerViewAsync(NavigationTarget target, ViewIdentifier viewIdentifier);
+    Task<IView> GetViewAsync(NavigationTarget target, ViewIdentifier viewIdentifier);
 }
