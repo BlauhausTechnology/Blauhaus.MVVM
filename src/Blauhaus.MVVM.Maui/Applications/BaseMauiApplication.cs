1ac3f7306e12fd6a35ee911cbcd7b1979ad46d55
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-07-28 11:33:49 +0200

    no message

--- src/Blauhaus.MVVM.Maui/Applications/BaseMauiApplication.cs
+++ src/Blauhaus.MVVM.Maui/Applications/BaseMauiApplication.cs
@@ -1,9 +1,12 @@
 ﻿using Blauhaus.Analytics.Abstractions;
+using Blauhaus.Analytics.Abstractions.Extensions;
+using Blauhaus.Errors;
 using Blauhaus.Ioc.Abstractions;
 using Blauhaus.Ioc.DotNetCoreIocService;
 using Blauhaus.MVVM.Abstractions.Application;
 using Blauhaus.MVVM.Abstractions.Navigator;
 using Blauhaus.MVVM.AppLifecycle;
+using Blauhaus.MVVM.Ioc;
 using Blauhaus.MVVM.Services;
 using Microsoft.Extensions.Logging;
 
@@ -27,12 +30,11 @@ public abstract class BaseMauiApplication<TApplication> : Application
         IViewNavigator viewNavigator)
     {
         ServiceLocator = serviceLocator;
+        AppServiceLocator.Initialize(serviceLocator.Resolve<IServiceLocator>);
         Logger = logger;
         ViewNavigator = viewNavigator;
         _appLifecycleService = ServiceLocator.ResolveAs<AppLifecycleService>(typeof(IAppLifecycleService));
         MainPage = new ContentPage();
-        
-        AppServiceLocator.Initialize(serviceLocator.Resolve<IServiceLocator>);
     }
 
     protected sealed override async void OnStart()
@@ -42,6 +44,22 @@ public abstract class BaseMauiApplication<TApplication> : Application
         Logger.SetValue("AppSessionId", Guid.NewGuid());
         _analyticsSession = Logger.BeginTimedScope(LogLevel.Information, "App Session");
         Logger.LogTrace("Application starting");
+
+        var startupTasks = ServiceLocator.Resolve<IEnumerable<StartupTasks>>();
+        foreach (var startupTask in startupTasks)
+        {
+            foreach (Func<Task> task in startupTask)
+            {
+                try
+                {
+                    await task.Invoke();
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError(Error.Unexpected("Startup task threw an exception"), e);
+                }
+            }
+        }
 
         _appLifecycleService.NotifyAppStarting();
         
