31d25d064510d4c7b82fbc05ed5b1a61c42a5961
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-08-18 16:07:03 +0200

    no message

--- src/Blauhaus.MVVM.TestHelpers/MockBuilders/Services/ViewNavigatorMockBuilder.cs
+++ src/Blauhaus.MVVM.TestHelpers/MockBuilders/Services/ViewNavigatorMockBuilder.cs
@@ -2,6 +2,7 @@
 using Blauhaus.MVVM.Abstractions.Navigator;
 using Blauhaus.MVVM.Navigation;
 using Blauhaus.TestHelpers.MockBuilders;
+using Moq;
 
 namespace Blauhaus.MVVM.TestHelpers.MockBuilders.Services;
 
@@ -11,12 +12,24 @@ public class ViewNavigatorMockBuilder : BaseMockBuilder<ViewNavigatorMockBuilder
     {
         Mock.Verify(x => x.NavigateAsync(viewTarget));
     }   
+    public void VerifyNavigateAsync(IViewTarget viewTarget, Times times)
+    {
+        Mock.Verify(x => x.NavigateAsync(viewTarget), times);
+    }   
     public void VerifyNavigateAsync(ViewIdentifier viewIdentifier)
     {
         Mock.Verify(x => x.NavigateAsync(ViewTarget.Create(viewIdentifier)));
     }
+    public void VerifyNavigateAsync(ViewIdentifier viewIdentifier, Times times)
+    {
+        Mock.Verify(x => x.NavigateAsync(ViewTarget.Create(viewIdentifier)), times);
+    }
     public void VerifyGoBackAsync()
     {
         Mock.Verify(x => x.GoBackAsync());
+    }
+    public void VerifyGoBackAsync(Times times)
+    {
+        Mock.Verify(x => x.GoBackAsync(), times);
     }
 }
